75703fa2808613927a8458a5658dcd30a16bbeb5
Aitor Rodriguez Gallardo <Aitor.gallardo.14@gmail.com>
2023-02-02 20:35:38 +0100

    Hecho el laberinto, el añadir torres y puesto el castillo, con un import desde la asset store

--- Assets/Scripts/TowerLocation.cs
+++ Assets/Scripts/TowerLocation.cs
@@ -1,10 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class TowerLocation : MonoBehaviour
 {
     public GameObject towerPrefab;
 
-
+    // Executes when click and unclick the GameObject with the mouse
+    private void OnMouseUpAsButton()
+    {
+        // Build Tower above TowerLocation
+        GameObject newTower = (GameObject) Instantiate(towerPrefab);
+        newTower.transform.position = transform.position + Vector3.up;
+    }
 }
